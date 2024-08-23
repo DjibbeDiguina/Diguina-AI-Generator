893309921bfb22c9dae6d88470ac0c4659a20467
DjibbeDiguina <diguinafils1@gmail.com>
2024-08-23 15:48:15 +0200

    generate ai content

--- app/(data)/ListTemplate.tsx
+++ app/(data)/ListTemplate.tsx
@@ -1,7 +1,7 @@
 export default  [
     {
         name:'Blog Title',
-        desc:'An AI tool that generate blog title depends on yout blog information',
+        desc:'An AI tool that generate blog title depends on your blog information',
         category:'Blog',
         icon:'https://cdn-icons-png.flaticon.com/128/4186/4186534.png',
         aiPrompt:'Give me 5 blog topic idea in bullet wise only based on give niche & outline and give me result in Rich text editor format',
